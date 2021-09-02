cfdacde3e4a75a40d629abf58a4e5ba873f5480d
paulanthonybegley <paulanthonybegley@gmail.com>
2021-09-02 23:59:18 +0200

    Updates

--- _next/static/qWS9j8_deoC3jgBBY3L9G/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-92a1f70d0eaf829e96eb.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/[slug]":["static/chunks/pages/[slug]-4e2e0864da3e215475ad.js"],sortedPages:["/","/_app","/_error","/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
